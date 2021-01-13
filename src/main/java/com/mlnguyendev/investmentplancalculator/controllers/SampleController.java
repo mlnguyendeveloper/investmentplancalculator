2b93d72d4195d97c606179fff0c5420b4dde14f2
mlnguyendeveloper <mlnguyendev@gmail.com>
2021-01-12 21:54:01 -0500

    Updated controllers to return those test pages

--- src/main/java/com/mlnguyendev/investmentplancalculator/controllers/SampleController.java
+++ src/main/java/com/mlnguyendev/investmentplancalculator/controllers/SampleController.java
@@ -8,16 +8,16 @@ public class SampleController {
 	
 	@GetMapping("/")
 	public String home() {
-		return("<h1>Welcome</h1>");
+		return("index");
 	}
 	
 	@GetMapping("/user")
 	public String user() {
-		return ("<h1>Welcome User</h1>");
+		return ("user-test-page");
 	}
 	
 	@GetMapping("/admin")
 	public String admin() {
-		return ("<h1>Welcome Admin</h1>");
+		return ("admin-test-page");
 	}
 }
