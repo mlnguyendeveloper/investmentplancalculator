50076d22f4e0f87160517a1303e58c97215cbd21
mlnguyendeveloper <mlnguyendev@gmail.com>
2021-01-14 22:23:05 -0500

    Added UserService implementation

--- src/main/java/com/mlnguyendev/investmentplancalculator/service/UserService.java
+++ src/main/java/com/mlnguyendev/investmentplancalculator/service/UserService.java
@@ -8,6 +8,7 @@ public interface UserService {
 	
 	public List<User> findAll();
 	public User findById(int id);
+	public User findByUsername(String userName);
 	public void save(User user);
 	public void deleteById(int id);
 }
