e6ab942bd83999b5d5e32a414e2fe86506624b79
mlnguyendeveloper <mlnguyendev@gmail.com>
2021-01-25 22:43:04 -0500

    Added missing annotations to define Models as Entites

--- src/main/java/com/mlnguyendev/investmentplancalculator/model/Step.java
+++ src/main/java/com/mlnguyendev/investmentplancalculator/model/Step.java
@@ -1,10 +1,14 @@
 package com.mlnguyendev.investmentplancalculator.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="step")
 public class Step {
 	
 	@Id
