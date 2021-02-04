18e4275dc93cad49566883fb377fd6dd216e4dc0
mlnguyendeveloper <mlnguyendev@gmail.com>
2021-02-04 00:01:49 -0500

    Updated PlanService to add a new plan

--- src/main/java/com/mlnguyendev/investmentplancalculator/service/IPlanService.java
+++ src/main/java/com/mlnguyendev/investmentplancalculator/service/IPlanService.java
@@ -3,6 +3,8 @@ package com.mlnguyendev.investmentplancalculator.service;
 import java.util.List;
 
 import com.mlnguyendev.investmentplancalculator.model.Plan;
+import com.mlnguyendev.investmentplancalculator.model.PlanDTO;
+import com.mlnguyendev.investmentplancalculator.model.User;
 
 public interface IPlanService {
 	public List<Plan> findAll();
@@ -10,4 +12,5 @@ public interface IPlanService {
 	public Plan findByName(String name);
 	public void save(Plan plan);
 	public void deleteById(int id);
+	public void createPlan(PlanDTO planDTO, User user);
 }
--- src/main/java/com/mlnguyendev/investmentplancalculator/service/PlanService.java
+++ src/main/java/com/mlnguyendev/investmentplancalculator/service/PlanService.java
@@ -3,10 +3,15 @@ package com.mlnguyendev.investmentplancalculator.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.mlnguyendev.investmentplancalculator.dao.IPlanRepository;
 import com.mlnguyendev.investmentplancalculator.model.Plan;
+import com.mlnguyendev.investmentplancalculator.model.PlanDTO;
+import com.mlnguyendev.investmentplancalculator.model.User;
 
 public class PlanService implements IPlanService {
 
@@ -58,4 +63,18 @@ public class PlanService implements IPlanService {
 		planRepository.deleteById(id);
 	}
 
+	@Override
+	@Transactional
+	public void createPlan(PlanDTO planDTO, User user) {
+		Plan plan = new Plan();
+		
+		//Set properties from the PlanDTO
+		BeanUtils.copyProperties(planDTO, plan);
+		
+		//Saves the newly created plan
+		planRepository.save(plan);
+		
+		//Updates the user with a new plan
+		user.addPlan(plan);
+	}
 }
